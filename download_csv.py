dfda66f92f0b8d3c9dc01f6bf0a703aefd846072
Shliosen <tv.vaitkus@gmail.com>
2024-03-06 23:20:23 +0200

    created simple dash

--- download_csv.py
+++ download_csv.py
@@ -1,17 +1,29 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import yfinance as yf
+import plotly.express as px
 
+# Gauti duomenis apie Apple akcijas
+data = yf.download('AAPL', '2023-01-01', '2024-01-01').reset_index()
+data['performance'] = data['Open'] - data['Close']
 
-import pandas as pd
-from selenium import webdriver
-# from selenium.webdriver.chrome.options import ChromeOptions
+# Sukurti Dash aplikaciją
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
+# Sudėti HTML išdėstymą
+app.layout = dcc.Graph(
+    figure={
+        'data': [
+            {'x': data['Date'], 'y': data['performance'], 'type': 'bar'},
+        ],
+        'layout': {
+            'title': 'Daily Perfomance of Tesla'
+        }
+    }
+)
 
-# Atidarykite naršyklę ir eikite į norimą puslapį
-# options = ChromeOptions()
-browser = webdriver.Chrome()
-browser.get('https://www.marketwatch.com/investing/stock/tsla/download-data?mod=mw_quote_tab')
-
-# <<<<<<<< got blocked... neeeeeeeeeeed wait.....>>>>>>>>>>>
-
-
-
-
+# Paleisti aplikaciją
+if __name__ == '__main__':
+    app.run_server(debug=True)
